e666170ad7f357e6ab6cffe7ad3bb42d31f3edf5
NewDanpon <68070260@kmitl.ac.th>
2025-10-04 15:23:05 +0700

    tid arai wa
    kod yak

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const myinput = document.getElementById("myinput")
+let a = 0;
+const CurrentAccount = document.getElementById("Currentbox1");
+const CurrentCash = document.getElementById("Currentbox2");
+const log = document.getElementById("output-box");
+const Change = document.getElementById("Change");
 
-function add(){
-    myinput.value = Number(myinput.value) + 1
-}
-function remove(){
-    myinput.value = Number(myinput.value) - 1
+function Change() {
+    a++;
+    const newText = `${a}, Current account Balance: ${CurrentAccount.value}, Current cash Balance: ${CurrentCash.value}\n`;
+    log.value = newText + log.value; 
+    
 }
